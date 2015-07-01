a28bfa8ecaa707dfddc1f029b808994628e8bd9b
Shiraji <isogai.shiraji@gmail.com>
2015-07-01 14:48:05 +0900

    fix #4 Check if the editor component is active or not when user type.

--- src/com/github/shiraji/hidetoolwindowsex/handler/HideToolWindowsExHandler.java
+++ src/com/github/shiraji/hidetoolwindowsex/handler/HideToolWindowsExHandler.java
@@ -4,12 +4,15 @@ import com.github.shiraji.hidetoolwindowsex.util.HideToolWindowsExUtils;
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ex.ToolWindowManagerEx;
 import com.intellij.psi.PsiFile;
 
 public class HideToolWindowsExHandler extends TypedHandlerDelegate {
     @Override
     public Result charTyped(char c, Project project, Editor editor, PsiFile file) {
-        HideToolWindowsExUtils.hide(project);
+        if(ToolWindowManagerEx.getInstanceEx(project).isEditorComponentActive()) {
+            HideToolWindowsExUtils.hide(project);
+        }
         return Result.CONTINUE;
     }
 }
